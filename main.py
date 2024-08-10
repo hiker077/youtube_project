026d6cdf442538ac252a0ad5e8ef368b2dea7f6a
Arek K <arkadiusz.kostrzewa92@gmail.com>
2024-08-10 15:37:05 +0200

    Module data_processing was changed. Now returns dataframe. Main process was commented needs to be rebuilt.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from youtube_utils.api import get_video_list, get_video_statistics, get_video_categories
 from youtube_utils.config import *
 from youtube_utils.data_processing import prepare_and_save_data
+import pandas as pd 
 import json
 
 def main():
@@ -8,31 +9,36 @@ def main():
     ## set up long or medium
 
     # print('1. Dowload of video IDs started.')
-    # video_list = get_video_list(url =  URL_SEARCH, api_key = API_KEY, maxResults= MAX_RESULTS, channel_id= CHANNEL_ID, video_duration= VIDEO_DURATION)
-    
+    # video_list_long = get_video_list(url =  URL_SEARCH, api_key = API_KEY, maxResults= MAX_RESULTS, channel_id= CHANNEL_ID, video_duration= VIDEO_DURATION_LONG)
+    # video_list_medium = get_video_list(url =  URL_SEARCH, api_key = API_KEY, maxResults= MAX_RESULTS, channel_id= CHANNEL_ID, video_duration= VIDEO_DURATION_MEDIUM)
+    # video_list = video_list_long + video_list_medium
+
     # print('2. Saving od video IDs list to started.')
     # with open('data/raw_data/video_list.json', 'w') as file:
     #     json.dump(video_list, file)
 
-
-    #test
     ##Dowload video statistics
     ## Adding less URL_VIDEOS, parallel
-    video_statistics, category_list = get_video_statistics(url = URL_VIDEOS, channelId = CHANNEL_ID, videoList = video_list, api_key=API_KEY)
-    #Dowload video categories
-    video_categories = get_video_categories(url=URL_VIDEO_CATEGORIES, api_key= API_KEY, list_of_id= category_list)
+       ##dodaj ścieżki do CONFIG 
 
-    #Saving raw data t json. 
-    ## !!!Sprawdz czy zadziała lokalizacja w nowym miejscu
-    ##dodaj ścieżki do CONFIG 
-    with open('data/raw_data/video_statistics_list.json', 'w') as file:
-        json.dump(video_statistics, file)
+    # with open('data/raw_data/video_list.json', 'r') as file:
+    #     video_list = json.load(file)
 
-    with open('data/raw_data/video_categories.json', 'w') as file:
-        json.dump(video_categories, file)
+    # video_statistics, category_list = get_video_statistics(url = URL_VIDEOS, channelId = CHANNEL_ID, videoList = video_list, api_key=API_KEY)
+    # with open('data/raw_data/video_statistics_list.json', 'w') as file:
+    #     json.dump(video_statistics, file)
     
-    prepare_and_save_data('data/dashboard_data/', 'youtube_data_dashboard')
+
+    # # #Dowload video categories
+    # video_categories = get_video_categories(url=URL_VIDEO_CATEGORIES, api_key= API_KEY, list_of_id= category_list)
+    # with open('data/raw_data/video_categories.json', 'w') as file:
+    #     json.dump(video_categories, file)
+
+
+    youtube_data_dashboard = prepare_and_save_data(path_video_statistics_list =PATH_VIDEO_STATISTICS_LIST, path_videos_categories= PATH_VIDEOS_CATEGORIES)
+    youtube_data_dashboard.to_csv(PATH_DASHBOARD_DATA + 'youtube_data_dashboard.csv')
     
+
 
     
 if __name__ == "__main__":
