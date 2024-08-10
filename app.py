8919dfcbc6446d87495899d77146a4be1200489f
Arek K <arkadiusz.kostrzewa92@gmail.com>
2024-08-10 16:30:50 +0200

    Add app.py file and dashboard_utils directory

---
+++ app.py
@@ -0,0 +1,44 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import dash_bootstrap_components as dbc
+import pandas as pd
+
+df = pd.read_csv('youtube_data_dashboard.csv')
+#count po grupie peirod 
+grouped_df = df.groupby(['PUBLISHED_PERIOD']).size().reset_index(name='Count')
+fig = px.bar(grouped_df, x='PUBLISHED_PERIOD', y='Count')
+
+
+external_stylesheets = [dbc.themes.COSMO]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = dbc.Container([
+    dbc.Row([
+        html.Div('My First App with Data, Graph, and Controls', className="text-primary text-center fs-3")
+    ]),
+    dbc.Row([
+        dbc.Col(html.Div("One of three columns")),
+        dbc.Col([ 
+            html.Div("The second of three columns", className="text-center my-3"),
+            dcc.Graph(figure=fig, id='my-first-graph-final')
+        ])
+    ])
+], fluid=True)
+
+##https://dash-example-index.herokuapp.com/
+
+# insporacja: 
+# https://mahmoud2227.pythonanywhere.com/
+
+### Next time 
+# @callback(
+#     Output(component_id='my-first-graph-final', component_property='figure'),
+#     Input(component_id='radio-buttons-final', component_property='value')
+# )
+# def update_graph(col_chosen):
+#     fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+#     return fig
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
