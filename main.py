8b179a3355bf2b6ec1ae767750161c723d1c3b3c
Arek K <arkadiusz.kostrzewa92@gmail.com>
2025-01-09 09:12:23 +0100

    working on missing env settings

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-import pandas as pd
 from pathlib import Path
 from dotenv import dotenv_values
 from dash import Dash
@@ -11,43 +10,48 @@ from src.dashboard.layout import create_layout
 from src.dashboard.callbacks import register_callbacks
 from src.dashboard.utilities import load_data
 
-EXTERNAL_STYLESHEETS = [dbc.themes.BOOTSTRAP, "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.7.2/font/bootstrap-icons.min.css"]
+EXTERNAL_STYLESHEETS = [
+    dbc.themes.BOOTSTRAP,
+    "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.7.2/font/bootstrap-icons.min.css",
+]
 config = dotenv_values(".env")
-dashboard_data_path = Path(config['DASHBOARD_DATA_FILE'])
+dashboard_data_path = Path(config["DASHBOARD_DATA_FILE"])
 
-#Download of raw data
-print('1. Dowload of video IDs started.')
-video_list_long = get_video_list(url =  config['URL_SEARCH'], api_key = config['API_KEY'], channel_id= config['CHANNEL_ID'],published_after= config['PUBLISHED_AFTER'], published_before = config['PUBLISHED_BEFORE'],  video_duration= config['VIDEO_DURATION_LONG'])
-video_list_medium = get_video_list(url =  config['URL_SEARCH'], api_key = config['API_KEY'], channel_id= config['CHANNEL_ID'], published_after= config['PUBLISHED_AFTER'], published_before= config['PUBLISHED_BEFORE'], video_duration= config['VIDEO_DURATION_MEDIUM'])
-video_list = video_list_long + video_list_medium
-with open(config['FILE_VIDEO_LIST'], 'w') as file: 
-    json.dump(video_list_long, file)
+# #Download of raw data
+# print('1. Dowload of video IDs started.')
+# video_list_long = get_video_list(url =  config['URL_SEARCH'], api_key = config['API_KEY'], channel_id= config['CHANNEL_ID'],published_after= config['PUBLISHED_AFTER'], published_before = config['PUBLISHED_BEFORE'],  video_duration= config['VIDEO_DURATION_LONG'])
+# video_list_medium = get_video_list(url =  config['URL_SEARCH'], api_key = config['API_KEY'], channel_id= config['CHANNEL_ID'], published_after= config['PUBLISHED_AFTER'], published_before= config['PUBLISHED_BEFORE'], video_duration= config['VIDEO_DURATION_MEDIUM'])
+# video_list = video_list_long + video_list_medium
+# with open(config['FILE_VIDEO_LIST'], 'w') as file:
+#     json.dump(video_list_long, file)
 
-print('3. Saving of videos statistics list has been started.')
-video_statistics, category_list = get_video_statistics(url = config['URL_VIDEOS'], channelId = config['CHANNEL_ID'], videoList = video_list, api_key= config['API_KEY'])
-with open(config['PATH_VIDEO_STATISTICS_LIST'], 'w') as file:
-    json.dump(video_statistics, file)
-    
-print('4. Saving of videos categories list has been started.')
-video_categories = get_video_categories(url=config['URL_VIDEO_CATEGORIES'], api_key= config['API_KEY'], category_ids= category_list)
-with open(config['PATH_VIDEOS_CATEGORIES'], 'w') as file:
-    json.dump(video_categories, file)
+# print('3. Saving of videos statistics list has been started.')
+# video_statistics, category_list = get_video_statistics(url = config['URL_VIDEOS'], channelId = config['CHANNEL_ID'], videoList = video_list, api_key= config['API_KEY'])
+# with open(config['PATH_VIDEO_STATISTICS_LIST'], 'w') as file:
+#     json.dump(video_statistics, file)
 
-#Data preparation
-data = prepare_and_save_data(config['PATH_VIDEO_STATISTICS_LIST'], config['PATH_VIDEOS_CATEGORIES'])
-data.to_csv(config['DASHBOARD_DATA_FILE'])
+# print('4. Saving of videos categories list has been started.')
+# video_categories = get_video_categories(url=config['URL_VIDEO_CATEGORIES'], api_key= config['API_KEY'], category_ids= category_list)
+# with open(config['PATH_VIDEOS_CATEGORIES'], 'w') as file:
+#     json.dump(video_categories, file)
+
+# #Data preparation
+# data = prepare_and_save_data(config['PATH_VIDEO_STATISTICS_LIST'], config['PATH_VIDEOS_CATEGORIES'])
+# data.to_csv(config['DASHBOARD_DATA_FILE'])
 
 # Run of dashboard
 if dashboard_data_path.exists():
     data = load_data(dashboard_data_path)
 else:
-    raise ValueError(f'File with necessary data does not exist: {dashboard_data_path.name}')
+    raise ValueError(
+        f"File with necessary data does not exist: {dashboard_data_path.name}"
+    )
 
 
-app = Dash(__name__, external_stylesheets= EXTERNAL_STYLESHEETS)
+app = Dash(__name__, external_stylesheets=EXTERNAL_STYLESHEETS)
 app.title = "YouTube Dashboard"
-app.layout = create_layout(data, config['YOUTUBE_LOGO'])
+app.layout = create_layout(data, config["YOUTUBE_LOGO"])
 
 register_callbacks(app)
-if __name__ == '__main__':
-     app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run_server(debug=True)
